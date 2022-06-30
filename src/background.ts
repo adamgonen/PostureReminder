749c45cdcc2c36f7be84df3e1a0398a50cd8c0a3
Adam Gonen <adam.gonen123@gmail.com>
2022-06-30 16:27:00 -0700

    update to use alarms and storage api
    This should be more reliable and fix many of the problems people are
    having but will require using more permissions.

--- src/background.ts
+++ src/background.ts
@@ -1,41 +1,42 @@
-let currentNotification: number;
+import Alarm = chrome.alarms.Alarm;
 
-window.addEventListener("storage", () => {
-  makeOrDeleteNotification();
-});
+const ALARM_NAME = "PostureReminderAlarm";
 
-const makeNotification = () => {
-  chrome.notifications.create(
-    {
-      type: "basic",
-      iconUrl: "icons/icon128.png",
-      title: "Posture Reminder",
-      message: "Correct your posture!",
-    },
-    () => {
-      makeOrDeleteNotification();
-    }
-  );
+const makeNotification = (alarm: Alarm) => {
+  if (alarm.name !== ALARM_NAME) {
+    return;
+  }
+  chrome.notifications.create({
+    type: "basic",
+    iconUrl: "icons/icon128.png",
+    title: "Posture Reminder",
+    message: "Correct your posture!",
+  });
 };
+
+const updateAlarm = async () => {
+  await chrome.alarms.clear(ALARM_NAME);
+  chrome.alarms.onAlarm.removeListener((alarm) => makeNotification(alarm));
+  chrome.storage.sync.get(null, (items) => {
+    if (items.enabled) {
+      let time: string = items.time;
+      if (time === "1000") {
+        time = getRandomTime().toString();
+      }
+      chrome.alarms.create(ALARM_NAME, {
+        periodInMinutes: parseInt(time),
+      });
+      chrome.alarms.onAlarm.addListener((alarm) => makeNotification(alarm));
+    }
+  });
+};
+
+updateAlarm();
+
+chrome.storage.onChanged.addListener(async () => {
+  await updateAlarm();
+});
 
 const getRandomTime = (): number => {
   return Math.floor(Math.random() * 240) + 30;
-};
-
-const makeOrDeleteNotification = () => {
-  if (window.localStorage.getItem("enabled") === "true") {
-    let time = window.localStorage.getItem("time") as string;
-    if (time === "1000") {
-      time = getRandomTime().toString();
-    }
-    if (currentNotification) {
-      window.clearTimeout(currentNotification);
-    }
-    currentNotification = window.setTimeout(
-      makeNotification,
-      parseInt(time) * 1000 * 60
-    );
-  } else {
-    window.clearTimeout(currentNotification);
-  }
 };
--- src/popup.ts
+++ src/popup.ts
@@ -2,28 +2,34 @@ let enabled: boolean;
 const button = document.getElementById("toggle") as HTMLInputElement;
 const select = document.getElementById("time") as HTMLOptionElement;
 
-if (localStorage.getItem("enabled") === null) {
-  localStorage.enabled = "false";
-  enabled = false;
-}
-if (localStorage.getItem("time") === null) {
-  localStorage.time = "60";
-}
+chrome.storage.sync.get(null, (items) => {
+  if (items.enabled === null) {
+    chrome.storage.sync.set({ enabled: "false" });
+  } else {
+    enabled = items.enabled;
+  }
+  button.checked = enabled;
 
-enabled = localStorage.enabled === "true";
-button.checked = enabled;
+  if (items.time === null) {
+    chrome.storage.sync.set({ time: "60" });
+  }
+});
 
-button.onclick = () => {
-  enabled = !enabled;
-  localStorage.enabled = enabled;
-};
+button.addEventListener("click", () => {
+  enabled = button.checked;
+  chrome.storage.sync.set({ enabled: enabled }, () => {
+    console.log(chrome.runtime.lastError);
+  });
+});
 
-if (localStorage.time) {
-  selectElement("time", localStorage.time);
-}
+chrome.storage.sync.get("time", (items) => {
+  if (items.time) {
+    selectElement("time", items.time);
+  }
+});
 
 select.addEventListener("change", () => {
-  localStorage.time = select.value;
+  chrome.storage.sync.set({ time: select.value });
 });
 
 function selectElement(id: string, valueToSelect: string) {
